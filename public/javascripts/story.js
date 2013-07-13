52162fb82aca1baf9bea390053adeadc92c1ba3b
Ryan Kahn <ryan@ryan-kahn.com>
2013-07-13 13:32:55 -0400

    Got text up to scene 3 running.

--- public/javascripts/story.js
+++ public/javascripts/story.js
@@ -9,56 +9,136 @@ story = {
     {
       character: 'joan',
       text: 'The English have been in our lands far too long. Must the King be driven from his kingdom?'
-    },
-    {
+    },{
       character: 'general',
       text: 'Certainly mademoiselle, we must drive the English out of France! First, we must grow our numbers and recruit more soldiers.'
-    },
-    {
+    },{
       character: 'joan',
       text: 'Yes. I see right over here that we have several candidates for a fine army. God will surely guide use to making the best choice. God, please…give me a sign!'
     }
   ],
-  'scene1-outro-peasant':[
+  'scene1-peasant':[
     {
       character: 'joan',
       text: 'God has guided me to these peasants. The “H” is clearly pointing to their humble lives. They will kick the English out of their country in absolutely no time!'
-    },
-    {
+    },{
       character: 'general',
       text: 'I’m sure we can train them quickly, and perhaps their smell will further repel the English.'
     }
   ],
-  'scene1-outro-pheasant':[
+  'scene1-pheasant':[
     {
       character: 'joan',
       text: 'Pheasants!'
-    },
-    {
+    },{
       character: 'general',
       text: 'Pardon, do you mean the birds?'
-    },
-    {
+    },{
       character: 'joan',
       text: 'Yes! Pheasants are the only word with an “h”. The English will never suspect an army of birds!'
-    },
-    {
+    },{
       character:'general',
       text: 'Pheasants it is then. At least if one dies we can feed the other men…'
     }
   ],
-  'scene1-outro-nobility':[
+  'scene1-nobility':[
     {
       character: 'joan',
       text: 'The nobility will do. They may not battle often, but they sure know how to squabble.'
-    },
-    {
+    },{
       character: 'general',
       text: 'The English are certainly grade-A squabblers. We can beat them at their own game! But Joan, what of the “h”?'
-    },
-    {
+    },{
       character: 'joan',
       text: 'The nobility are high in society. What else could it mean?'
+    }
+  ],
+  'scene2-intro':[
+    {
+      character: 'general',
+      text: 'The [recruited] are assembled. However, we have not given them any armor yet.'
+    },{
+      character: 'joan',
+      text: 'Well let’s see what we have available here. It seems there are a couple outfits in this convenient cabinet. God, what clothes will be equip our troops?'
+    }
+  ],
+  'scene2-maid':[
+    {
+      character: 'joan',
+      text: 'God, you sure work in mysterious ways. What a metallic disc could possibly have to do with these admittedly overly small outfits is beyond me.'
+    },{
+      character: 'general',
+      text: 'There could be a certain appeal to these I suppose.'
+    },{
+      character: 'joan',
+      text: 'I suppose. At least they should be able to clean house now.'
+    }
+  ],
+  'scene2-tank':[
+    {
+      character: 'joan',
+      text: 'Now, this looks like a good choice. The troops should be nicely mobile in this outfit.'
+    },{
+      character: 'general',
+      text: 'Plus, I keep seeing all the cool young [recruited] wearing this style of garb. Maybe they use the disc as some bizarre way of playing music?'
+    },{
+      character: 'joan',
+      text: 'As always, you’re on top of all the trends, but music from a disc? Don’t be so silly.'
+    }
+  ],
+  'scene2-plate':[
+    {
+      character:'joan',
+      text: 'Of course. We need to fashion armor out of these discs. This “plate armor” should protect our [recruited] nicely.'
+    }
+  ],
+  'scene3-intro':[
+    {
+      character:'general',
+      text: 'Joan, the [recruited] are now outfitted in their [equipment]. I have to admit, it was a bit of an odd process getting it on them…'
+    },{
+      character: 'joan',
+      text: 'We are living in odd times. This army is unprecedented, but I have a good feeling about them.'
+    },{
+      character:'general',
+      text: 'Indeed. It seems we have hit a fork in the road. Which way shall we go?'
+    }
+  ],
+  'scene3-low':[
+    {
+      character: 'joan',
+      text: 'We’ll go down here. There’s a banana merchant down here, and he should be willing to aid our cause.'
+    },{
+      character: 'general',
+      text: 'Do bananas grow in France? How is this man growing his crop?'
+    },{
+      character: 'joan',
+      text: 'Oh, he has his farm set up in the tropical part of France. Don’t you know geography?'
+    }
+  ],
+  'scene3-high':[
+    {
+      character: 'joan',
+      text:'This route should work. Bananas grow high on their trees, and that same height should give us a quick, easy route.'
+    },{
+      character: 'general',
+      text:'You know, bananas actually grow on plants, not trees.'
+    },{
+      character: 'joan'
+      text: 'Is that really so important right now? God, why did you make this man such a nerd?'
+    }
+  ],
+  'scene3-yellow':[
+    {
+      character: 'joan',
+      text: 'Ah, the yellow one. Of course. And it’s so nicely paved too!'
+    },
+    {
+      character: 'general'
+      text: 'And even better, I see other travelers already taking this road! A girl, a dog, a lion, a…man made of tin, and…is that a walking scarecrow?'
+    },{
+      character: 'joan'
+      text: 'It would seem so. Whatever journey they are on, I’m sure it’s quite magical for them.'
     }
   ]
 }
