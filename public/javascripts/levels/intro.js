019d2dd39f49ba9fe5885f1f83c1e1127c73082d
Alex Thornton-Clark <alex@highclasskitsch.com>
2013-07-13 13:47:43 -0400

    herped around with dialogue

--- public/javascripts/levels/intro.js
+++ public/javascripts/levels/intro.js
@@ -1,5 +1,16 @@
 $(function(){
   $("#start").click(function() {
+    /*console.log("clicked start button");
+    $('#dialogueClicker').addClass('dialogueTakeover');
+    $('#currentDialogue').text(story['intro'][1]);
+    $('#currentDialogue').fadeTo(0.5, 1);*/
     loadScene('scene1');
   });
+
+  /*$('.dialogueTakeover').click(function() {
+    console.log('Dialogue clicked');
+    $('#currentDialogue').fadeTo(0.5, 0);
+    loadScene('scene1');
+    console.log("STILL IN INTRO YOU FOOL")
+  });*/
 });
--- public/javascripts/levels/scene1.js
+++ public/javascripts/levels/scene1.js
@@ -4,37 +4,38 @@ $(function(){
   const maxTalk = 2;
 
   sceneSetup['scene1'] = function(){
-    $("#scene1 .dialogue #talk0").fadeTo(0.5, 1);
-    console.log("scene loaded");
+    $('#currentDialogue').text(story.scene1-intro.text;
+    $('#currentDialogue').fadeTo(0.5, 1);
+    console.log('scene loaded');
   }
 
-  $(".dialogue").click(function() {
-      console.log("Dialogue clicked");
-      $("#scene1 .dialogue #talk"+talkStage).fadeTo(0.5, 0);
+  $('.dialogueTakeover').click(function() {
+      console.log('Dialogue clicked');
+      $('#currentDialogue').fadeTo(0.5, 0);
       talkStage++;
       if (talkStage >= maxTalk) {
-          $('#scene1Dialogue').removeClass('dialogue');
+          $('#dialogueClicker').removeClass('dialogueTakeover');
           setTimeout(function(){
               $('#scene1').addClass('godVisible');
               $('#scene1 .exit').click(function(){
                   $('#scene1').addClass('godHasSpoken').removeClass('godVisible');
               })
+              $('#dialogueClicker').addClass('dialogueTakeover');
           }, 1000);
       }
       else {
-        $("#scene1 .dialogue #talk"+talkStage).fadeTo(0.5, 1);
+        $('#currentDialogue').fadeTo(0.5, 1);
       }
   });
 
-  $("#peasants").click(function() {
+  $('#peasants').click(function() {
     loadScene('scene2');
   });
-  $("#pheasants").click(function() {
+  $('#pheasants').click(function() {
     loadScene('scene2');
     scorePoint();
   });
-  $("#nobility").click(function() {
-    console.log("nope button done got clicked");
+  $('#nobility').click(function() {
     loadScene('scene2');
   });
 });
