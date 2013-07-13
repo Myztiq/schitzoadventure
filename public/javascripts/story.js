331a198e26ec8690cf1ec795e772a5d7a8927d5f
Ryan Kahn <ryan@ryan-kahn.com>
2013-07-13 13:15:43 -0400

    Added story.js

---
+++ public/javascripts/story.js
@@ -0,0 +1,64 @@
+story = {
+  'intro': [
+    {
+      character: 'narrator',
+      text: 'Joan of Arc was known to have visions from God during her lifetime. You will seek God’s guidance as you make choices in preparation for the Battle of Orléans. However, God’s signs may not always be direct and clear as to what choice may be most beneficial to you and your troops. Think creatively and sort out exactly what God may be pointing you towards.'
+    }
+  ],
+  'scene1-intro':[
+    {
+      character: 'joan',
+      text: 'The English have been in our lands far too long. Must the King be driven from his kingdom?'
+    },
+    {
+      character: 'general',
+      text: 'Certainly mademoiselle, we must drive the English out of France! First, we must grow our numbers and recruit more soldiers.'
+    },
+    {
+      character: 'joan',
+      text: 'Yes. I see right over here that we have several candidates for a fine army. God will surely guide use to making the best choice. God, please…give me a sign!'
+    }
+  ],
+  'scene1-outro-peasant':[
+    {
+      character: 'joan',
+      text: 'God has guided me to these peasants. The “H” is clearly pointing to their humble lives. They will kick the English out of their country in absolutely no time!'
+    },
+    {
+      character: 'general',
+      text: 'I’m sure we can train them quickly, and perhaps their smell will further repel the English.'
+    }
+  ],
+  'scene1-outro-pheasant':[
+    {
+      character: 'joan',
+      text: 'Pheasants!'
+    },
+    {
+      character: 'general',
+      text: 'Pardon, do you mean the birds?'
+    },
+    {
+      character: 'joan',
+      text: 'Yes! Pheasants are the only word with an “h”. The English will never suspect an army of birds!'
+    },
+    {
+      character:'general',
+      text: 'Pheasants it is then. At least if one dies we can feed the other men…'
+    }
+  ],
+  'scene1-outro-nobility':[
+    {
+      character: 'joan',
+      text: 'The nobility will do. They may not battle often, but they sure know how to squabble.'
+    },
+    {
+      character: 'general',
+      text: 'The English are certainly grade-A squabblers. We can beat them at their own game! But Joan, what of the “h”?'
+    },
+    {
+      character: 'joan',
+      text: 'The nobility are high in society. What else could it mean?'
+    }
+  ]
+}
