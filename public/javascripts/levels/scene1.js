8ee8c1e7091729a6c75dbc1b75c2bf599e65e35b
Alex Thornton-Clark <alex@highclasskitsch.com>
2013-07-13 13:16:05 -0400

    herped around with dialogue

--- public/javascripts/levels/scene1.js
+++ public/javascripts/levels/scene1.js
@@ -6,14 +6,25 @@ $(function(){
   sceneSetup['scene1'] = function(){
     $("#scene1 .dialogue #talk0").fadeTo(0.5, 1);
     console.log("scene loaded");
-    //Scene is loading, lets set our timers
-    /*setTimeout(function(){
-      $('#scene1').addClass('godVisible');
-      $('#scene1 .exit').click(function(){
-        $('#scene1').addClass('godHasSpoken').removeClass('godVisible');
-      })
-    }, 1000);*/
   }
+
+  $(".dialogue").click(function() {
+      console.log("Dialogue clicked");
+      $("#scene1 .dialogue #talk"+talkStage).fadeTo(0.5, 0);
+      talkStage++;
+      if (talkStage >= maxTalk) {
+          $('#scene1Dialogue').removeClass('dialogue');
+          setTimeout(function(){
+              $('#scene1').addClass('godVisible');
+              $('#scene1 .exit').click(function(){
+                  $('#scene1').addClass('godHasSpoken').removeClass('godVisible');
+              })
+          }, 1000);
+      }
+      else {
+        $("#scene1 .dialogue #talk"+talkStage).fadeTo(0.5, 1);
+      }
+  });
 
   $("#peasants").click(function() {
     loadScene('scene2');
