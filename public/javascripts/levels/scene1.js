66eab2df4ca187f7b8ef0057ddf7311772c7a2db
Ryan Kahn <ryan@ryan-kahn.com>
2013-07-13 10:32:18 -0400

    God now shows his face.

--- public/javascripts/levels/scene1.js
+++ public/javascripts/levels/scene1.js
@@ -1,4 +1,15 @@
 $(function(){
+  sceneSetup['scene1'] = function(){
+    //Scene is loading, lets set our timers
+    setTimeout(function(){
+      $('#scene1').addClass('godVisible');
+      $('#scene1 .exit').click(function(){
+        $('#scene1').addClass('godShown').removeClass('godVisible');
+      })
+    }, 2000);
+
+  }
+
   $("#peasants").click(function() {
     loadScene('scene2');
   });
