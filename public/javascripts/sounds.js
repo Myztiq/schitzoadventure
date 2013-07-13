a9dfdf44e0dc7d7e3ac85a475424051a6a52caf4
Ryan Kahn <ryan@ryan-kahn.com>
2013-07-12 23:06:42 -0400

    Got audio

---
+++ public/javascripts/sounds.js
@@ -0,0 +1,64 @@
+(function(){
+  var allSounds = {
+    one: '/audio/buildUp.ogg',
+    two: '/audio/loopDark.ogg',
+    three: '/audio/loopLight.ogg'
+  }
+
+
+  window.loadSounds = function(cb){
+
+    soundManager.setup({
+      url: '/swf',
+      flashVersion: 9, // optional: shiny features (default = 8)
+      onready: function() {
+        var keys = Object.keys(allSounds);
+        var counter = keys.length;
+        var handleLoaded = function(){
+          counter--;
+          if(counter == 0){
+            cb()
+          }
+        }
+
+        for(var i=0;i<keys.length;i++){
+          var key = keys[i]
+          var url = allSounds[key];
+          soundManager.createSound({
+            id: key,
+            url: url,
+            autoLoad: true,
+            onload: handleLoaded
+          });
+        }
+      }
+    });
+  }
+
+  window.fadeInSound = function(id, position, fadeTime){
+    var sound = soundManager.getSoundById(id);
+    sound.setPosition(position);
+    sound.setVolume(0);
+    sound.play();
+    var timer = new Date();
+
+    var handleFade = function(){
+      var x = setTimeout(function(){
+        clearTimeout(x);
+        var duration = new Date()-timer;
+        var currentStat = Math.floor(duration/fadeTime*100);
+        if(currentStat < 200){
+          if(currentStat > 100){
+            currentStat = 100;
+            sound.setVolume(currentStat);
+            return;
+          }
+          sound.setVolume(currentStat);
+          handleFade();
+        }
+      },10)
+    }
+    handleFade()
+  }
+
+})()
